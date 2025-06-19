3b081d2a506204e921b7b4e598abcb050f55c078
sgr-8426 <sagarnarigara69@gmail.com>
2025-06-19 16:06:57 +0530

    portfolio website

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/portfolio-website/', // <-- Set base to match GitHub Pages repo
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
